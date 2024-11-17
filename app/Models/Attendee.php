fada6bcf6c8f0f0c83d1e1cea9eae015769d8d7b
far1h <farihmuhammad45@gmail.com>
2024-11-17 10:19:45 +0700

    Define model relationships

--- app/Models/Attendee.php
+++ app/Models/Attendee.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Attendee extends Model
 {
-    //
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function attendees(){
+        return $this->belongsTo(Attendee::class);
+    }
 }
