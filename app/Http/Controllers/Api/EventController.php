c356d669321130dd00f23e71bfdb5e9d6399213e
far1h <farihmuhammad45@gmail.com>
2024-11-20 11:01:06 +0700

    Add protection rules to endpoints

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -7,12 +7,21 @@ use App\Http\Resources\EventResource;
 use App\Http\Traits\CanLoadRelationships;
 use App\Models\Event;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\Middleware;
+use Illuminate\Routing\Controllers\HasMiddleware;
 
-class EventController extends Controller
+class EventController extends Controller implements HasMiddleware
 {
     use CanLoadRelationships;
+    private $relations = ["user", "attendees", "attendees.user"];
 
-    private array $relations = ['user', 'attendees', 'attendees.user'];
+    public static function middleware(): array
+    {
+        return [
+            new Middleware('auth:sanctum', except: ['index', 'show']),
+        ];
+    }
+
 
     /**
      * Display a listing of the resource.
@@ -37,7 +46,7 @@ class EventController extends Controller
                 'start_time' => 'required|date',
                 'end_time' => 'required|date|after:start_time'
             ]),
-            'user_id' => 1
+            'user_id' => $request->user()->id
         ]);
         return new EventResource($this->loadRelationships($event));
     }
